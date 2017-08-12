a4ca463005535552165af5a53836c58545345df2
Alexander Egorov <acegorov7@gmail.com>
2017-08-12 07:39:39 -0400

    put everything inside a grid (looks a lot better now); added live WPM

--- just_type/just_type.py
+++ just_type/just_type.py
@@ -26,27 +26,29 @@ class JustType(Frame):
         self.right_cnt = StringVar()
         self.wrong_cnt = StringVar()
         self.time_or_wpm = StringVar()
-        self.tests = StringVar()
+        self.live_wpm = StringVar()
+        self.test = IntVar()
 
         self.text = Text(self, font=("Courier", 20), width=50, height=2)
-        self.entry = Entry(self, textvariable=self.typing, font=("Times New Roman", 20), takefocus=0)
-        self.right_word_label = Label(self, foreground='green', font=("Courier", 15))
-        self.wrong_word_label = Label(self, foreground='red', font=("Courier", 15))
-        self.countdown_label = Label(self, font=("Courier", 15))
+        self.entry = Entry(self, textvariable=self.typing, font=("Courier", 20), takefocus=0)
+        self.right_word_label = Label(self, foreground='green', font=("Calibri", 15))
+        self.wrong_word_label = Label(self, foreground='red', font=("Calibri", 15))
+        self.countdown_label = Label(self, font=("Calibri", 15))
+        self.live_wpm_label = Label(self, font=("Calibri", 15))
         self.reset_button = Button(self, text='Reset', command=self.reset)
-        self.test_options = ttk.Combobox(self, state="readonly")
+        self.radio_easy = Radiobutton(self, text='Easy', variable=self.test, value=0, command=self.change_test)
+        self.radio_advanced = Radiobutton(self, text='Advanced', variable=self.test, value=1, command=self.change_test)
 
         self.right_word_label['textvariable'] = self.right_cnt
         self.wrong_word_label['textvariable'] = self.wrong_cnt
         self.countdown_label['textvariable'] = self.time_or_wpm
-        self.test_options['textvariable'] = self.tests
+        self.live_wpm_label['textvariable'] = self.live_wpm
 
         self.right_cnt.set('Correct: {}'.format(self.right_words))
         self.wrong_cnt.set('Incorrect: {}'.format(self.wrong_words))
         self.time_or_wpm.set('Type to start!')
-        self.tests.set('Common')
-
-        self.test_options['values'] = ('Easy', 'Advanced')
+        # TODO: fix live_wpm to not mess with the grid when it changes number of digits
+        self.live_wpm.set('{} WPM'.format(self.cur_wpm))
 
         self.entry.focus_set()
 
@@ -60,16 +62,17 @@ class JustType(Frame):
         self.text.tag_config('right', foreground='green')
         self.text.tag_config('wrong', foreground='red')
 
-        self.text.pack()
-        self.entry.pack()
-        self.right_word_label.pack()
-        self.wrong_word_label.pack()
-        self.countdown_label.pack()
-        self.reset_button.pack()
-        self.test_options.pack()
+        self.text.grid(column=0, row=0, columnspan=3)
+        self.entry.grid(column=1 , row=1)
+        self.reset_button.grid(column=2 , row=1)
+        self.right_word_label.grid(column=1 , row=2)
+        self.wrong_word_label.grid(column=1 , row=3)
+        self.live_wpm_label.grid(column=2, row=3)
+        self.countdown_label.grid(column=1 , row=4)
+        self.radio_easy.grid(column=0 , row=1)
+        self.radio_advanced.grid(column=0 , row=2)
 
         root.bind('<KeyPress>', self.on_key_press)
-        self.test_options.bind('<<ComboboxSelected>>', self.change_test)
 
     def reset(self):
         self.text.config(state=NORMAL)
@@ -83,6 +86,7 @@ class JustType(Frame):
         self.right_cnt.set('Correct: {}'.format(self.right_words))
         self.wrong_cnt.set('Incorrect: {}'.format(self.wrong_words))
         self.time_or_wpm.set('Type to start!')
+        self.live_wpm.set('{} WPM'.format(self.cur_wpm))
 
     def reset_variables(self):
         self.cur_char = 0
@@ -93,13 +97,13 @@ class JustType(Frame):
         self.right_chars = 0
         self.wrong_words = 0
         self.wrong_chars = 0
+        self.cur_wpm = 0
 
         self.start_count = False
         self.stop = False
 
-    def change_test(self, event):
-        self.wordbank = words[self.test_options.current()]
-        # self.test_options.selection_clear()
+    def change_test(self):
+        self.wordbank = words[self.test.get()]
         self.reset()
 
     def init_rand_gen(self):
@@ -138,13 +142,20 @@ class JustType(Frame):
             total_chars += len(self.wordbank[random_num]) + 1
 
     def countdown(self, count):
-        if not self.start_count:   # prevents two timers from running at once
+        # prevents two timers from running at once
+        if not self.start_count:
             return
 
         self.time_or_wpm.set('{}'.format(math.ceil(count)))
 
         if count > 0:
-            self.after(100, self.countdown, count - 0.1)    # perhaps not the best way to do this- I'm running this function 10x more to prevent user from starting two timers up at once
+            self.after(100, self.countdown, count - 0.1)
+
+            # stops from dividing by zero, which would lead to first letter being marked incorrect
+            if count != self.timer_limit:
+                self.cur_wpm = int(self.right_chars * (60 / (self.timer_limit - count)) // 5)
+
+            self.live_wpm.set('{} WPM'.format(self.cur_wpm))
         else:
             wpm = int(self.right_chars * (60 / self.timer_limit) // 5)
 
@@ -167,20 +178,22 @@ class JustType(Frame):
         try:
             self.text.config(state=NORMAL)
 
-            if ord(event.char) == 8:        # backspace
+            # backspace
+            if ord(event.char) == 8:
                 self.remove_char()
 
-            elif ord(event.char) == 32:     # space
+            # space
+            elif ord(event.char) == 32:
                 if self.cur_letter == 0:
                     self.entry.delete(0, END)
                 else:
                     self.move_next_word()
 
             elif (event.char).isalpha():
-                if not self.start_count:    # if test timer hasn't started, start it
+                # if test timer hasn't started, start it
+                if not self.start_count:
                     self.start_count = True
                     self.countdown(self.timer_limit)
-
                 self.add_char(event.char)
 
             self.text.config(state=DISABLED)
@@ -235,7 +248,8 @@ class JustType(Frame):
             self.right_chars += len(self.wordbank[bank_index]) + 1
 
             for letter in self.wordbank[bank_index]:
-                if letter.istitle():  # counts upper-case letters as two characters
+                # counts upper-case letters as two characters
+                if letter.istitle():
                     self.right_chars += 1
         else:
             self.add_effect('wrong')
