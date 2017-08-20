1fbbbbcacd6a6b9fc165b6d83d993369d94f152c
Alexander Egorov <acegorov7@gmail.com>
2017-08-20 01:00:04 -0400

    added shebang

--- justtype/__main__.py
+++ justtype/__main__.py
@@ -1,8 +1,11 @@
-def main():
-	from just_type import JustType
-	root = JustType()
-	root.resizable(width=False, height=False)
-	root.mainloop()
+#!/usr/bin/python3
 
-if __name__ == "__main__":
-	main()
+
+def main():
+    from just_type import JustType
+    root = JustType()
+    root.resizable(width=False, height=False)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
